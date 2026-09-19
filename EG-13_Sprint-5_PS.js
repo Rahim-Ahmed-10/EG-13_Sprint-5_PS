e2b99b6ddd5f46926bc2f2ce0e57767ac5d9b672
Rahim-Ahmed-10 <rahimahmed01690@gmail.com>
2026-09-19 23:07:27 +0600

    Add All Problem

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,1 +1,265 @@
-<h2>Hello</h2>
+/**
+ * EG-13_Sprint-5_PS.js
+ */
+
+// ==========================================
+// 01. Remove Duplicates from Sorted Array
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
+// ==========================================
+// 02. Binary Search
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+
+// ==========================================
+// 03. Search Insert Position
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+// ==========================================
+// 04. Maximum Depth of Binary Tree
+// ==========================================
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth, rightDepth) + 1;
+};
+
+// ==========================================
+// 05. Invert Binary Tree
+// ==========================================
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return null;
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+    return root;
+};
+
+// ==========================================
+// 06. Product of Array Except Self
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let n = nums.length;
+    let result = new Array(n).fill(1);
+
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+};
+
+// ==========================================
+// 07. Rotate Array
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    
+    var reverse = function(arr, start, end) {
+        while (start < end) {
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    };
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+
+// ==========================================
+// 08. Min Stack
+// ==========================================
+/**
+ * @return {void}
+ */
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    let popped = this.stack.pop();
+    if (popped === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+// ==========================================
+// 09. Continuous Subarray Sum
+// ==========================================
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    let remainderMap = new Map();
+    remainderMap.set(0, -1);
+    let cumulativeSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        cumulativeSum += nums[i];
+        let remainder = cumulativeSum % k;
+        
+        if (remainder < 0) {
+            remainder += k;
+        }
+
+        if (remainderMap.has(remainder)) {
+            if (i - remainderMap.get(remainder) >= 2) {
+                return true;
+            }
+        } else {
+            remainderMap.set(remainder, i);
+        }
+    }
+
+    return false;
+};
+
+// ==========================================
+// 10. Daily Temperatures
+// ==========================================
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let n = temperatures.length;
+    let result = new Array(n).fill(0);
+    let stack = [];
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            let prevIndex = stack.pop();
+            result[prevIndex] = i - prevIndex;
+        }
+        stack.push(i);
+    }
+
+    return result;
+};
